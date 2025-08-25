7bea6bc242d0d5f82e8488b63e9606ce117e4170
manuel.suarez <c-manuel.suarez@khoros.com>
2025-08-25 14:58:54 -0300

    Create initial getTaskById() controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import type { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,11 +12,15 @@ export class TasksController {
     return this.tasksService.getAllTasks()
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id)
+  }
+
   @Post()
   createTask(
-    @Body('title') title: string, 
-    @Body('description') description: string
+    @Body() createTaskDto: CreateTaskDto
   ): Task {
-    return this.tasksService.createTask(title, description)
+    return this.tasksService.createTask(createTaskDto)
   }
 }
